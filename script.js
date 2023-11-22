9fa8f055739f2b55965fee44b69db03f0c6e6e46
Øystein <oystein.koder@gmail.com>
2023-11-22 11:51:17 +0100

    change

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-function resize_one_image (img) {
-    const img = document.getElementsByClassName ("#single_image_ID");
-    img.style.width = "500px";
-    img.style.height = "500px";
-    return img;
+image1 = document.querySelector ("#img1_ID");
+image1.addEventListener ("click", toggle_one_image);
+
+function toggle_one_image () {
+    let change = true;
+
+    if (change == true) {
+        image1.classList = "large_img";
+        change = false;
+    } else {
+        image1.classList = "small_img";
+        change = true;
+    }
 }
 
+image2 = document.querySelector ("#img2_ID");
+image2.addEventListener ("click", toggle_multiple_images);
+
+function toggle_multiple_images () {
+    image2.style.height = "20em";
+    image2.style.width = "20em";
+}
