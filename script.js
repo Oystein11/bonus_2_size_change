c884cbf8a0fc2a9e5925783d5ab79adbd9b6bfba
Øystein <oystein.koder@gmail.com>
2023-11-22 14:33:47 +0100

    Fixes

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 image1 = document.querySelector ("#img1_ID");
 image1.addEventListener ("click", toggle_one_image);
 
-function toggle_one_image () {
-    let change = true;
+let change = true;
 
+function toggle_one_image () {
     if (change == true) {
         image1.classList = "large_img";
         change = false;
@@ -19,10 +19,10 @@ image2.addEventListener ("click", toggle_multiple_images);
 image3 = document.querySelector ("#img3_ID");
 image3.addEventListener ("click", toggle_multiple_images);
 
-function toggle_multiple_images () {
-    let change2 = true;
+let change2 = true;
 
-    if (change2 = true) {
+function toggle_multiple_images () {
+    if (change2 == true) {
         image2.classList = "large_img";
         image3.classList = "large_img";
         change2 = false;
