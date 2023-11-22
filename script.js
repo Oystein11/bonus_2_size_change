b6f7666ecb7d5afe852d0419cc76c30fd18c1d45
Øystein <oystein.koder@gmail.com>
2023-11-22 10:24:30 +0100

    init

---
+++ script.js
@@ -0,0 +1,7 @@
+function resize_one_image (img) {
+    const img = document.getElementsByClassName ("#single_image_ID");
+    img.style.width = "500px";
+    img.style.height = "500px";
+    return img;
+}
+
